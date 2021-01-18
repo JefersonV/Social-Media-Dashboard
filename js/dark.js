b1482070ce1b4c668dabe1d6c7ce162c831f7b2a
JefersonV <jvelasquezc15@miumg.edu.gt>
2021-01-18 01:13:23 -0600

    logica hecha para dark mode

--- js/dark.js
+++ js/dark.js
@@ -1,7 +1,21 @@
 const checkbox = document.querySelector('#checkbox')
 console.log(checkbox)
 console.log(window.matchMedia('(prefers-color-scheme: dark)'))
-
+//document.body.style.setProperty('--cardGray', 'skyblue')
 if(window.matchMedia('(preferes-color-scheme: dark)')) {
     checkbox.setAttribute('checkbox',true)
 }
+
+checkbox.addEventListener('change', function (event) {
+    
+
+    
+
+  if(this.checked) {
+    document.body.classList.remove('is-light-mode')
+    document.body.classList.add('is-dark-mode')
+  } else {
+    document.body.classList.remove('is-dark-mode')
+    document.body.classList.add('is-light-mode')
+  } 
+})
